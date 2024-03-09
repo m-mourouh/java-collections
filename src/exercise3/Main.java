cd29f66a33ee9524b5c6077078a5838bd81ed5f4
MOHAMED MOUROUH <60442896+m-mourouh@users.noreply.github.com>
2024-03-09 22:40:00 +0100

    Java collections: add exercise 3

---
+++ src/exercise3/Main.java
@@ -0,0 +1,35 @@
+package exercise3;
+
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet<String> groupeA = new HashSet<>();
+        HashSet<String> groupeB = new HashSet<>();
+
+        //Add students names to groupeA
+        groupeA.add("Ahmed");
+        groupeA.add("Rachid");
+        groupeA.add("Mariam");
+        groupeA.add("John");
+        groupeA.add("Mark");
+
+        //Add students names to groupeB
+        groupeB.add("Ahmed");
+        groupeB.add("Alexis");
+        groupeB.add("Mariam");
+        groupeB.add("Markos");
+        groupeB.add("Alexandre");
+
+        //Intersection between groupeA and groupeB
+        HashSet<String> intersection = new HashSet<>(groupeA);
+        intersection.retainAll(groupeB);
+        System.out.println("Intersection: " + intersection);
+
+        //Union between groupeA and groupeB
+        HashSet<String> union = new HashSet<>(groupeA);
+        union.addAll(groupeB);
+        System.out.println("Union: " + union);
+
+    }
+}
